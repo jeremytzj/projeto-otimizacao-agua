a47b13cd758c3e06c4e5a3d6cd144be017b22586
Jeremy <100741107+jeremytzj@users.noreply.github.com>
2025-07-13 01:43:49 -0300

    le os parametros

--- alocation_model.py
+++ alocation_model.py
@@ -11,3 +11,38 @@ meses = energia['Mes'].tolist()
 sistemas = producao['Sistema Integrado'].tolist()
 municipio = distribuicao['Municipio'].unique().tolist()
 
+# parametros 
+
+pe = {
+    (col.strip(), row['Mes']): row[col]
+    for _, row in energia.iterrows()
+    for col in energia.columns if col != 'Mes' 
+}
+
+pt = {
+    (col.strip(), row['Mes']): row[col]
+    for _, row in tratamento.iterrows()
+    for col in tratamento.columns if col != 'Mes' 
+}
+
+S = {}
+for _, i in producao.iterrows():
+    sys, vol = i['Sistema Integrado'], i['Volume Maximo']   
+    S[sys] = vol
+
+D = {}
+for _, i in demanda.iterrows():
+    mes, vol = i['Mes'], i['Volume Minimo']   
+    D[mes] = vol
+
+v = {}
+for _, i in distribuicao.iterrows():
+    sys, mun, vol = i['Sistema Integrado'], i['Municipio'], i['Volume Maximo'] 
+    v[(sys,mun)] = vol
+
+
+
+
+
+
+
