5a7bd9edf0b6c5540f70295a8123fa01cf870c71
Jeremy <100741107+jeremytzj@users.noreply.github.com>
2025-07-13 02:53:59 -0300

    Add pyomo model and add parameter data in the dados.xlsx sheet

--- alocation_model.py
+++ alocation_model.py
@@ -5,6 +5,7 @@ tratamento = pd.read_excel("./dados/dados.xlsx", sheet_name="custo_tratamento")
 producao = pd.read_excel("./dados/dados.xlsx", sheet_name="producao_maxima")
 demanda = pd.read_excel("./dados/dados.xlsx", sheet_name="demanda_minima")
 distribuicao = pd.read_excel("./dados/dados.xlsx", sheet_name="distribuicao_maxima")
+demanda_municipio = pd.read_excel('./dados/dados.xlsx', sheet_name="demanda_municipios")
 
 #conjuntos
 meses = energia['Mes'].tolist()
@@ -30,10 +31,17 @@ for _, i in producao.iterrows():
     sys, vol = i['Sistema Integrado'], i['Volume Maximo']   
     S[sys] = vol
 
-D = {}
-for _, i in demanda.iterrows():
-    mes, vol = i['Mes'], i['Volume Minimo']   
-    D[mes] = vol
+D = {
+    (row['Municipio'], col) : row[col]
+    for _, row in demanda_municipio.iterrows()
+    for col in demanda_municipio.columns if col != 'Municipio' 
+}
+
+d = {}
+for _,i in demanda.iterrows():
+    d[i["Mes"]] = i["Volume Minimo"]
+d
+
 
 v = {}
 for _, i in distribuicao.iterrows():
@@ -41,8 +49,57 @@ for _, i in distribuicao.iterrows():
     v[(sys,mun)] = vol
 
 
+# inicializa o modelo
 
+from pyomo.environ import *
 
+model = ConcreteModel()
 
+#conjuntos
+model.T = Set(initialize=meses)
+model.I = Set(initialize=sistemas)
+model.J = Set(initialize=municipio)
 
+#parametros
+model.pe = Param(model.I, model.T, initialize=pe)
+model.pt = Param(model.I, model.T, initialize=pt)
+model.S = Param(model.I, initialize=S)
+model.D = Param(model.J, model.T, initialize=D, default = 0)
+model.v = Param(model.I, model.J, initialize=v, default = 0)
+model.d = Param(model.T, initialize=d)
 
+#variaveis de decisao
+model.x = Var(model.I, model.J, model.T, domain=NonNegativeReals)
+
+# funcao objetiva
+def obj(model):
+    return sum(model.x[i,j,t]*(model.pe[i,t] + model.pt[i,t]) for i in model.I for j in model.J for t in model.T)
+
+model.obj = Objective(rule=obj, sense=minimize)
+
+#restricoes
+model.producao = ConstraintList()
+for i in model.I:
+    for t in model.T:
+        model.producao.add(expr=sum(model.x[i,j,t] for j in model.J) <= model.S[i])
+
+model.distribuicao = ConstraintList()
+for j in model.J:
+    for t in model.T:
+        model.distribuicao.add(expr=sum(model.x[i,j,t] for i in model.I) >= model.D[j,t]) 
+
+model.fluxo_maximo = ConstraintList()
+for i in model.I:
+    for j in model.J:
+        for t in model.T:
+            model.fluxo_maximo.add(expr=model.x[i,j,t]<= model.v[i,j])
+
+model.volume_minimo = ConstraintList()
+for t in model.T:
+    model.volume_minimo.add(expr=sum(model.x[i,j,t] for i in model.I for j in model.J) >= model.d[t])
+#resolvendo o modelo
+
+import gurobipy
+
+solver = SolverFactory('gurobi')
+solver.solve(model)
